071a80036ba5eb85e434c954b04f2678ad99755f
641krishna <shrikrishnaaluri641@gmail.com>
2022-06-18 14:08:15 +0530

    added signup page

---
+++ sportsData.js
@@ -0,0 +1,326 @@
+var data = [{
+    category: "clycling",
+    name: "Swimming Goggles - 6 Blue",
+    image_url: "https://contents.mediadecathlon.com/p1622777/k$6bce0491b5eb8200e12bd79bbc6d4bb9/swimming-goggles-size-l-clear-lenses-spirit-white-blue.jpg?format=auto&quality=70&f=300x0",
+    review: "22 reviews",
+    discount: "2,037.99",
+    off: "11% Off",
+    price: 1808.45,
+    deals: "Upgrade Deals",
+    productId:"women1",
+}, {
+    category: "clycling",
+    name: "Steering Damper",
+    image_url: "https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/16/AE/281948b9-0b56-4e3f-9166-6d6c5a9085a7.jpg.webp",
+    review: "5 reviews",
+    discount: "1,282.90",
+    off: "47% Off",
+    price: 678.83,
+    deals: "Upgrade Deals",
+    productId:"women2",
+}, {
+    category: "running",
+    name: "Running Shoes",
+    image_url:  "https://contents.mediadecathlon.com/p2174716/k$1e54ed7fc169abff9480eeca0569ab7b/men-s-running-shoes-run-100-grey.jpg?format=auto&quality=70&f=300x0",
+    review: "1 review",
+    discount: "2,264.52",
+    off: "51% Off",
+    price: 1104.70,
+    deals: "Upgrade Deals",
+    productId:"women3",
+}, {
+    category: "running",
+    name: "Walking Shoes",
+    image_url: "https://contents.mediadecathlon.com/p2175064/k$a323df0c88381a47cf8c79a3a38aeb5f/men-s-running-shoes-active-grip-green.jpg?format=auto&quality=70&f=300x0",
+    review: "0 review",
+    discount: "1,355.39",
+    off: "17% Off",
+    price: 1129.37,
+    deals: "Upgrade Deals",
+    productId:"women4",
+}, {
+    category: "sports wears",
+    name: "Sports Wears",
+    image_url: "https://contents.mediadecathlon.com/p2056307/k$c3b170b1591881390ed661b5fe81317b/men-gym-shorts-polyester-fst-100-black.jpg?format=auto&quality=70&f=300x0",
+    review: "5 reviews",
+    discount: "1360.40",
+    off: "31% Off",
+    price: 755,
+    deals: "Upgrade Deals",
+    productId:"women5",
+}, {
+    category: "gym",
+    name: "Dumbells for men",
+    image_url: "https://contents.mediadecathlon.com/p188429/k$65603f96431052a47fe56fe72fdf86d1/gym-hex-dumbbell-5-kg-black.jpg?format=auto&quality=70&f=300x0",
+    review: "0 review",
+    discount: "2,560.86",
+    off: "21% Off",
+    price: 1843.60,
+    deals: "Upgrade Deals",
+    productId:"women6",
+}, {
+    category: "gym",
+    name: "Gyms Tshirt",
+    image_url: "https://contents.mediadecathlon.com/p1962387/k$1e2d52236ded9a07292750c71a273ab5/men-cotton-blend-gym-t-shirt-regular-fit-500-grey-print.jpg?format=auto&quality=70&f=300x0",
+    review: "3 reviews",
+    discount: "1,813.37",
+    off: "9% Off",
+    price: 1643.08,
+    deals: "Upgrade Deals",
+    productId:"women7",
+}, {
+    category: "cycling",
+    name: "Roadster for men",
+    image_url: "https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/69/9F/b004b458-6263-4345-8a33-9b1b6b635a9f.jpg.webp",
+    review: "6 reviews",
+    discount: "6,651.09",
+    off: "17% Off",
+    price: 5517.21,
+    deals: "Upgrade Deals",
+    productId:"women8",
+}, {
+    category: "sports wears",
+    name: "Fishing lure",
+    image_url: "https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/EE/7A/927dd808-445e-4638-8d55-a85d0fddafb2.jpg.webp",
+    review: "6 reviews",
+    discount: "5139.78",
+    off: "18% Off",
+    price: 4132.20,
+    deals: "Upgrade Deals",
+    productId:"women9",
+}, {
+    category: "running",
+    name: "Onemix New Running Shoes",
+    image_url: "https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/5D/1D/27f73dbc-09be-45d6-8240-b306a86b0587.jpg.webp",
+    review: "5 reviews",
+    discount: "4,836.91",
+    off: "17% Off",
+    price: 4005.44,
+    deals: "Upgrade Deals",
+    productId:"women10",
+}, {
+    category: "sports wears",
+    name: "Women Bumboo Wears",
+    image_url: "https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/AD/A7/0a258698-c21b-4b17-bf75-f99f97937ec3.jpg.webp",
+    review: "10 reviews",
+    discount: "4,005.44",
+    off: "31% Off",
+    price: 2765.78,
+    deals: "Upgrade Deals",
+    productId:"women11",
+}, {
+    category: "gym",
+    name: "Mens Gym Shorts",
+    image_url: "https://contents.mediadecathlon.com/p2170985/k$ccbce2e5ce472c063e12600a7b047391/men-gym-shorts-with-zip-pocket-black.jpg?format=auto&quality=70&f=300x0",
+    review: "1 review",
+    discount: "3778.81",
+    off: "41% Off",
+    price: 2191.31,
+    deals: "Upgrade Deals",
+    productId:"women12",
+}, {
+    category: "gym",
+    name: "Womens Gym Wears",
+    image_url: "https://contents.mediadecathlon.com/p1705298/k$db286aa46dafc9745152988c15f2de0c/women-gym-leggings-polyester-high-waist-fti500a-black.jpg?format=auto&quality=70&f=300x0",
+    review: "1 review",
+    discount: "3,551.91",
+    off: "17% Off",
+    price: 2947.20,
+    deals: "Upgrade Deals",
+    productId:"women13",
+}, {
+    category: "cycling",
+    name: "New Hero cycle",
+    image_url: "https://contents.mediadecathlon.com/p1670555/k$ac8a7f35f2c32fe8ffd847e6f5f14274/adult-hybrid-cycle-riverside-120-grey.jpg?format=auto&quality=70&f=300x0",
+    review: "3 reviews",
+    discount: "3,476.32",
+    off: "16% Off",
+    price: 2871.61,
+    deals: "Upgrade Deals",
+    productId:"women14",
+}, {
+    category: "cycling",
+    name: "New Hero cycle for kids",
+    image_url: "https://contents.mediadecathlon.com/p2311372/k$8e62a68fd75bc85f9fa1ab631b182c25/kids-cycle-rockrider-st120-6-8-years-20inch-light-blue.jpg?format=auto&quality=70&f=300x0",
+    review: "9 reviews",
+    discount: "3,476.32",
+    off: "17% Off",
+    price: 2871.61,
+    deals: "Upgrade Deals",
+    productId:"women15",
+}, {
+    category: "cycling",
+    name: "Women's lady bird cycle ",
+    image_url: "https://contents.mediadecathlon.com/p2277240/k$a00a35dea99e3a6e6e0d6200666b8749/adult-mountain-bike-rockrider-st30-mint.jpg?format=auto&quality=70&f=300x0",
+    review: "11 reviews",
+    discount: "3,476.32",
+    off: "17% Off",
+    price: 2871.61,
+    deals: "Upgrade Deals",
+    productId:"women16",
+}, {
+    category: "gym",
+    name: "womens sports wear",
+    image_url: "https://contents.mediadecathlon.com/p1621994/d0b421cf2371ea85e9796e716a28b20e/p1621994.jpg?format=auto&quality=70&f=768x0",
+    review: "9 reviews",
+    discount: "3,476.32",
+    off: "17% Off",
+    price: 2871.61,
+    deals: "Upgrade Deals",
+    productId:"women17",
+}, {
+    category: "running",
+    name: "ladies Running wears",
+    image_url: "https://contents.mediadecathlon.com/p1954405/k$8f9acab1eba0c6b651a742e4a0486315/women-s-cotton-gym-t-shirt-regular-fit-sportee-light-pink.jpg?format=auto&quality=70&f=300x0",
+    review: "0 review",
+    discount: "3,400.81",
+    off: "18% Off",
+    price: 2796.02,
+    deals: "Upgrade Deals",
+    productId:"women18",
+}, {
+    category: "sports wears",
+    name: "Runnig T Shirt",
+    image_url: "https://contents.mediadecathlon.com/p2007587/k$1af17a4140952d21fffe546e4cbb84d9/women-s-cotton-gym-t-shirt-regular-fit-sportee-black.jpg?format=auto&quality=70&f=300x0",
+    review: "4 reviews",
+    discount: "3,400.81",
+    off: "18% Off",
+    price: 2796.02,
+    deals: "Upgrade Deals",
+    productId:"women19",
+}, {
+    category: "gym",
+    name: "Women Letter Pattern Casual V-Neck Long Sleeve T-Shirts - Dark Grey M",
+    image_url: "https://contents.mediadecathlon.com/p1932798/k$78235d60815fa0709d83741c81ba98d4/women-s-cycling-short-sleeved-jersey-500-mint-glow.jpg?format=auto&quality=70&f=300x0",
+    review: "5 reviews",
+    discount: "3,325.14",
+    off: "55% Off",
+    price: 1511.02,
+    deals: "Upgrade Deals",
+    productId:"women20",
+}, ]
+// localStorage.setItem("womens_data", JSON.stringify(data))
+
+// window.onload = (event) => {
+//     // console.log('page is fully loaded');
+//     displayData(data)
+//   };
+   displayData(data)
+let cartLS=JSON.parse(localStorage.getItem("addToCart"))||[]
+
+function displayData(data){
+    
+    let filter =document.querySelector("#filter")
+    
+   
+    document.getElementById("container").innerHTML=""
+    data.forEach(function(elem){ 
+       
+    let box=document.createElement("div")
+        let avtar=document.createElement("img")
+        avtar.setAttribute("src",elem.image_url)
+        let productName= document.createElement("h5")
+        productName.innerText=elem.name
+        let productPrice=document.createElement("p")
+       productPrice.innerText=elem.price
+        
+        let addCart=document.createElement("button")
+        addCart.innerText="Add to Cart"
+       
+        addCart.addEventListener("click",function(){
+        if( addToCart(elem.productId)===true){
+            alert("Product Added Successfully")
+            cartLS.push(elem);
+            localStorage.setItem("addToCart",JSON.stringify(cartLS))
+        }else{
+           alert("Product is already in the cart")
+        }
+       
+       })
+
+        box.append(avtar,productName,productPrice,addCart)
+        document.querySelector("#container").append(box)
+      
+        filter.addEventListener("change",function(){
+            if (filter.value==="all"){
+                displayData(data)
+             window.location.reload();
+             }
+           else{
+                let filtered=data.filter(function(element){
+                    return element.category===filter.value;
+                   
+                })
+               
+                displayData(filtered)
+            }
+        })
+
+     
+    })
+    // filter.addEventListener("change",function(){
+    //     if (filter.value==="all"){
+    //         displayData(data)
+        
+    //      }
+    //    else{
+
+            
+            
+    //         let filtered=data.filter(function(element){
+    //             return element.category===filter.value;
+    //         })
+            
+    //         displayData(filtered)
+    //     }
+    // })
+
+ 
+    
+
+       function addToCart(id){
+        for(let i=0;i<cartLS.length;i++){
+            if(cartLS[i].productId==id){
+                return false
+            }
+        }
+        return true;
+
+        }
+        // sort by price...............................
+        let sortt=document.querySelector("#price_tag")
+        sortt.addEventListener("change",shortedPrice)
+   
+        function shortedPrice() {
+            var valuePrice = document.querySelector("#price_tag").value;
+            if (valuePrice == "High") {
+                var sortedList = data.sort((a, b) => {
+                    return b.price - a.price;
+                })
+                displayData(sortedList);
+            } else if (valuePrice == "Low") {
+                var sortedList = data.sort((a, b) => {
+                    return a.price - b.price;
+                })
+                displayData(sortedList);
+            } else {
+                window.location.reload();
+            }
+        }
+       
+}
+// ------------------------------------slide..................
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
